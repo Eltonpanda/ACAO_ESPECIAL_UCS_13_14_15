1feb1dda27e0ef1500f9f892847a821b3029a7bd
Elton Marques <eltonmarque@gmail.com>
2023-10-01 16:56:35 -0300

    Codigo da aula de reforço das UCs 13, 14 e 15

---
+++ Projeto_API_23_09/Projeto_API_23_09/Controllers/LivroController.cs
@@ -0,0 +1,39 @@
+using ChapterFST1.Models;
+using ChapterFST1.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChapterFST1.Controllers
+{
+    [Produces("application/json")]
+
+    [Route("api/[controller]")]
+
+    [ApiController]
+
+    public class LivroController : ControllerBase
+    {
+        private readonly LivroRepository _livroRepository;
+
+        public LivroController(LivroRepository livroRepository)
+        {
+            _livroRepository = livroRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            try
+            {
+                return Ok(_livroRepository.Listar());
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+
+    }
+}
